a03ada10e1487fd8653c0f84402b700cf618f0b6
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-07-31 19:40:42 +0300

    Fix d7vk build source folder

--- source/repo_build.sh
+++ source/repo_build.sh
@@ -40,7 +40,7 @@ then
         fi
 
         case "$REPO_NAME" in
-            "dxvk"|"d7vk")
+            "dxvk")
                 BUILD_BASE_PATH="dxvk"
                 REPO_URL="https://github.com/doitsujin/dxvk"
                 ;;
@@ -61,7 +61,7 @@ then
                 REPO_URL="https://github.com/doitsujin/dxvk-tests"
                 ;;
             "d7vk")
-                BUILD_BASE_PATH="dxvk"
+                BUILD_BASE_PATH="d7vk"
                 REPO_URL="https://github.com/WinterSnowfall/d7vk"
                 ;;
             "d7vk-tests")
